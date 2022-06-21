5bbf3663ce6be244b04036a5bcccc56279cfa2b6
baddipudiDebora <64851336+baddipudiDebora@users.noreply.github.com>
2022-06-20 22:11:03 -0400

    first commit

---
+++ RestSharpAPITest/RESTfulAPICallDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAPITest
+{
+  public  static class RESTfulAPICallDetails
+    {
+        public static string Verb;
+        public static bool RequiresJsonBody;
+        public static Dictionary<string, string> URLParamters;
+        public static string URL;
+        public static string ActionMethod;
+
+        public static bool ValidateURL()
+        {
+            bool result = true;
+
+            if (String.IsNullOrWhiteSpace(URL))
+            {
+                result = false;
+            }
+            else
+            {
+                if (URL.Contains("{") || URL.Contains("}"))
+                    result = false;
+            }
+
+            return result;
+        }
+    }
+}
